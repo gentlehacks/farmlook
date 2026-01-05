946012bc1b1ccf292e4a6710b0cf000f4c480d1b
gentlehacks <salihuadamu4678@gmail.com>
2026-01-05 15:53:12 +0100

    Add: sitemap and robot files for SEO

--- app/sitemap.js
+++ app/sitemap.js
@@ -1,11 +1,10 @@
 export default function sitemap() {
   return [
     {
-      url: 'https://farmlook-site.com.ng', // Your actual domain
+      url: 'https://farmlook.com.ng', 
       lastModified: new Date(),
       changeFrequency: 'weekly',
       priority: 1,
     },
-    // Add more routes here if you have them, like /about or /blog
   ]
 }
