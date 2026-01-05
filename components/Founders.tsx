19ae33c1edb3bc8233e6a82512492b09fc3c8039
Pixelxus <salihuadamu4678@gmail.com>
2026-01-05 08:40:39 +0100

    Change image source from aliyu.png to aliyu.PNG

--- components/Founders.tsx
+++ components/Founders.tsx
@@ -30,7 +30,7 @@ const Founders = () => {
         {/* Sparker */}
         <div className="w-11 h-11 bg-green-200 border-1 border-green-300 rounded-full overflow-hidden">
           <Image
-            src="/aliyu.png"
+            src="/aliyu.PNG"
             alt="salihu adamu"
             width={200}
             height={200}
